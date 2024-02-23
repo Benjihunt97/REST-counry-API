02a149dc6caa9e5255e010c02765fd7418889457
Benjihunt97 <73445088+Benjihunt97@users.noreply.github.com>
2024-02-23 14:07:27 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,67 @@
+$(document).ready(() => {
+
+    // toggle the theme button
+    const themeBtn = $('.toggle-theme');
+    const moon = $('#dark');
+    const sun = $('#light');
+
+    themeBtn.click((e) => {
+        moon.toggleClass('-top-9');
+        sun.toggleClass('bottom-1');
+        $(e.currentTarget).toggleClass('bg-slate-900');
+        $('.change').toggleClass('dark-theme');
+        $('.change-btn').toggleClass('dark-theme-btn');
+    });
+
+
+    // show the filter item and add the animation to show them
+    const regions = [
+        'Africa',
+        'America',
+        'Asia',
+        'Europe'
+    ];
+
+    regions.forEach((item, index) => {
+        const regionItem = `
+            <div class="p-3 hover:bg-gray-200 transition-all cursor-pointer">
+                ${item}
+            </div>
+        `;
+
+        $('.filter-display').append(regionItem);
+    });
+
+    $('#show-regions').click((e) => {
+        $(e.currentTarget).find('svg').toggleClass('-rotate-90');
+        $('.filter-display').toggleClass('h-[200px]');
+    });
+
+    // fetching the data json file
+    $.ajax({
+        url: 'https://github.com/Benjihunt97/REST-counry-API/blob/main/data.json',
+        dataType: "json",
+        success: function (data) {
+            let flag = data.flags.svg;
+            let population = data.population;
+            let region = data.region;
+            let capital = data.capital;
+
+            let card = `
+            <div class="overflow-hidden rounded-md shadow-lg">
+                <img src="${flag}" alt="">
+                <div class="text-sm bg-white p-5">
+                    <h3 class="font-bold text-base">Germany</h3>
+                    <p>Population: <span class="population-txt">${population}</span></p>
+                    <p>Region: <span class="region-txt">${region}</span></p>
+                    <p>Capital: <span class="capital-txt">${capital}</span></p>
+                </div>
+            </div>
+        `;
+
+            $('.data-display').append(card);
+        }
+    });
+
+
+});
